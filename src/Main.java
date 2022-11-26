d5abf149d4e6c12c80534595bdf429e254209b9b
Tony <yaraliev5@gmail.com>
2022-11-26 22:44:55 +0300

    hw2.6

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        boolean success = Data.validate("name","name1","name1");
+
+        if (success) {
+            System.out.println("данные верны");
+
+        } else {
+            System.out.println("данные не верны");
+        }
+    }
+}
